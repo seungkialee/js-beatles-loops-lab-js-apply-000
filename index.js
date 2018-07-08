3fd07ec872dc775ad95b4fd15a06cab3c7870320
Anthony Lee <anthony.s.lee96@gmail.com>
2018-07-08 18:14:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function iLoveTheBeatles(num) {
   do {
     newArray.push('I love the Beatles!')
     num++
-  } while (num < 15)
-  
+  } while (num < 15);
+  return newArray;
 }  
 // add solution here
