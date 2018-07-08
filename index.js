e6cbd9db2947840c9c7ca092daeb3280c117d400
Anthony Lee <anthony.s.lee96@gmail.com>
2018-07-08 18:09:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function theBeatlesPlay(musicians, instruments) {
+var newArray = [];
+
+for (var i = 0; i < musicians.length; i++) {  
+  var string = `${musicians[i]} plays ${instruments[i]}` 
+  newArray.push(string)
+  
+  }
+  return newArray;
+} 
+
+
+function johnLennonFacts(string) {
+  var newArray = [];
+  
+  let i = 0;
+  while (i < string.length) {
+    newArray.push(`${string[i]}!!!`)
+    i++;
+  }
+  return newArray;
+}
+
+function iLoveTheBeatles() {
+  
+}
 // add solution here
